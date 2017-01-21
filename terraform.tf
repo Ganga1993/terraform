7dfd3fc9f8385c84fe545ac53e9fdbcfdb38a549
Ganga1993 <gangadharrao.pasupuleti@gmail.com>
2017-01-21 20:04:50 +0530

    security group

--- terraform.tf
+++ terraform.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "windows_ami"{
 	ami	= "${var.ami_name}"
 	instance_type = "${var.instance_type}"
 	key_name = "ganga_testing"
-	security_groups = "${split(",", var.security_groups)}"
+	vpc_security_group_ids = "${split(",", var.security_groups)}"
 	associate_public_ip_address = "true"
 	iam_instance_profile = "${var.iam_instance_pofile}"
 	tags{
